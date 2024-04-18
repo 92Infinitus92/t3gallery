f172e0833e25c77184f859713101b14b166099c5
Benjaminj Jones <benjaminj@dev-it.info>
2024-04-18 15:35:02 +0300

    implementing parallel routes

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,7 +1,15 @@
-export default function PhotoModal({
+import { getImage } from "../../../../server/db/queries";
+
+export default async function PhotoModal({
   params: { id: photoId },
 }: {
   params: { id: string };
 }) {
-  return <div>{photoId}</div>;
+  const idOfImage = parseInt(photoId);
+  const image = await getImage(idOfImage);
+  return (
+    <div>
+      <img src={image.url} alt={image.name} className="w-96 " />
+    </div>
+  );
 }
