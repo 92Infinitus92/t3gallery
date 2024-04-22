a4ef95ce783a4ffca3510d59429c977806dd7567
Benjaminj Jones <benjaminj@dev-it.info>
2024-04-22 18:04:16 +0300

    added toast for upload

--- src/app/_components/simple-upload.button.tsx
+++ src/app/_components/simple-upload.button.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useUploadThing } from "../../utils/uploadthing";
+import { toast } from "sonner";
 
 type Input = Parameters<typeof useUploadThing>;
 
@@ -50,7 +51,12 @@ export function SimpleUploadButton() {
   const router = useRouter();
 
   const { inputProps } = useUploadThingInputProps("imageUploader", {
+    onUploadBegin() {
+      toast("Uploading...", { duration: 3000, id: "upload-begin" });
+    },
     onClientUploadComplete() {
+      toast.dismiss("upload-begin");
+      toast("Upload complete");
       router.refresh();
     },
   });
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import Link from "next/link";
 import TopNav from "./_components/topnav";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { extractRouterConfig } from "uploadthing/server";
+import { Toaster } from "../components/ui/sonner";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -39,7 +40,8 @@ export default function RootLayout({
             <main className="overflow-y-scroll no-scrollbar">{children}</main>
           </div>
           {modal}
-          <div id="modal-root"></div>
+          <div id="modal-root" />
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
